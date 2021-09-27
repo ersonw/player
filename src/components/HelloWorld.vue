afa50f386291be5048fad98192bce7dea2aa20e0
ersonw <asoonnode@gmail.com>
2021-09-27 23:39:34 +0800

    202109272339

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,40 +1,43 @@
 <template>
   <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+    <vue-aliplayer-v2
+        ref="VueAliplayerV2"
+        :source="source"
+        :options="options"
+        style="width: 100%;height: 750px"
+    />
   </div>
 </template>
-
 <script>
+import VueAliplayerV2 from 'vue-aliplayer-v2'
 export default {
   name: 'HelloWorld',
+  comments: {
+    VueAliplayerV2
+  },
   props: {
     msg: String
+  },
+  data() {
+    return {
+      options: {
+        source: this.source,
+        isLive: false, // 切换为直播流的时候必填
+        format: 'dash' // 切换为直播流的时候必填
+      },
+      source: 'https://vs13.thisav.com/dash/OeVEbGzBr5xz02iiL1IhFg,1632757120/589052.mpd',
+    }
+  },
+  created() {
+    console.log(this.getQueryString('id'))
+  },
+  methods: {
+    getQueryString(name) {
+      var reg = new RegExp("(^|&)" + name + "=([^&]*)(&|$)", "i");
+      var r = window.location.search.substr(1).match(reg);
+      if (r != null) return unescape(r[2]);
+      return null;
+    }
   }
 }
 </script>
--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import App from './App.vue'
-
+import VueAliplayerV2 from 'vue-aliplayer-v2'
+Vue.use(VueAliplayerV2)
 Vue.config.productionTip = false
 
 new Vue({
